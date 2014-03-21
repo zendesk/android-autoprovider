26ffe2f9ed3a9b1fd4983fe307eb9fe475c8fb70
Jerzy Chalupski <jerzy@getbase.com>
2014-03-21 20:05:14 +0100

    Build AutoUris from ModelGraph

--- src/main/java/com/getbase/android/autoprovider/AutoUris.java
+++ src/main/java/com/getbase/android/autoprovider/AutoUris.java
@@ -1,8 +1,23 @@
 package com.getbase.android.autoprovider;
 
+import com.getbase.autoindexer.DbTableModel;
+import com.getbase.forger.thneed.MicroOrmModel;
+
+import org.chalup.thneed.ModelGraph;
+
 import android.net.Uri;
 
-public class AutoUris implements ModelUriBuilder {
+public class AutoUris<TModel extends DbTableModel & MicroOrmModel> implements ModelUriBuilder {
+  private final ModelGraph<TModel> mModelGraph;
+
+  private AutoUris(ModelGraph<TModel> modelGraph) {
+    mModelGraph = modelGraph;
+  }
+
+  public static <T extends DbTableModel & MicroOrmModel> AutoUris from(ModelGraph<T> modelGraph) {
+    return new AutoUris(modelGraph);
+  }
+
   @Override
   public ModelUri model(Class<?> klass) {
     return null;
