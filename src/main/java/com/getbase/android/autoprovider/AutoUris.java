6a453eaaf73d61268d5d9749b9fa15bdbbbcfc40
Jerzy Chalupski <jerzy@getbase.com>
2014-03-21 20:32:40 +0100

    It should be possible to specify default id column for AutoUris

--- src/main/java/com/getbase/android/autoprovider/AutoUris.java
+++ src/main/java/com/getbase/android/autoprovider/AutoUris.java
@@ -6,16 +6,37 @@ import com.getbase.forger.thneed.MicroOrmModel;
 import org.chalup.thneed.ModelGraph;
 
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 public class AutoUris<TModel extends DbTableModel & MicroOrmModel> implements ModelUriBuilder {
   private final ModelGraph<TModel> mModelGraph;
+  private final String mIdColumnName;
 
-  private AutoUris(ModelGraph<TModel> modelGraph) {
+  AutoUris(ModelGraph<TModel> modelGraph, String idColumnName) {
     mModelGraph = modelGraph;
+    mIdColumnName = idColumnName;
   }
 
-  public static <T extends DbTableModel & MicroOrmModel> AutoUris<T> from(ModelGraph<T> modelGraph) {
-    return new AutoUris<T>(modelGraph);
+  public static <T extends DbTableModel & MicroOrmModel> Builder<T> from(ModelGraph<T> modelGraph) {
+    return new Builder<T>(modelGraph);
+  }
+
+  public static class Builder<TModel extends DbTableModel & MicroOrmModel> {
+    private final ModelGraph<TModel> mModelGraph;
+    private String mIdColumnName = BaseColumns._ID;
+
+    Builder(ModelGraph<TModel> modelGraph) {
+      mModelGraph = modelGraph;
+    }
+
+    public Builder<TModel> defaultIdColumn(String idColumnName) {
+      mIdColumnName = idColumnName;
+      return this;
+    }
+
+    public AutoUris<TModel> build() {
+      return new AutoUris<TModel>(mModelGraph, mIdColumnName);
+    }
   }
 
   @Override
