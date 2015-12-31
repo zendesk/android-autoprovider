2d06e99716d6528f5f0ca937b9525f590690401f
Jerzy Chalupski <jerzy@getbase.com>
2015-12-31 12:19:17 +0100

    Remove unnecessary modifiers

--- library/src/main/java/com/getbase/android/autoprovider/ContentUriHandler.java
+++ library/src/main/java/com/getbase/android/autoprovider/ContentUriHandler.java
@@ -5,14 +5,14 @@ import android.database.Cursor;
 import android.net.Uri;
 
 public interface ContentUriHandler {
-  public boolean canHandle(Uri uri, ContentUriAction action);
-  public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder);
-  public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs);
-  public Uri insert(Uri uri, ContentValues values);
-  public int delete(Uri uri, String selection, String[] selectionArgs);
-  public String getType(Uri uri);
+  boolean canHandle(Uri uri, ContentUriAction action);
+  Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder);
+  int update(Uri uri, ContentValues values, String selection, String[] selectionArgs);
+  Uri insert(Uri uri, ContentValues values);
+  int delete(Uri uri, String selection, String[] selectionArgs);
+  String getType(Uri uri);
 
-  public static enum ContentUriAction {
+  enum ContentUriAction {
     QUERY,
     UPDATE,
     INSERT,
