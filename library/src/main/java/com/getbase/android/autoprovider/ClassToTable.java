535878ed106b27fa3e98216fe8f500c94a2dd690
Jerzy Chalupski <jerzy@getbase.com>
2016-01-04 10:57:21 +0100

    Remove duplicated code

--- library/src/main/java/com/getbase/android/autoprovider/ClassToTable.java
+++ library/src/main/java/com/getbase/android/autoprovider/ClassToTable.java
@@ -3,27 +3,14 @@ package com.getbase.android.autoprovider;
 import com.google.common.collect.ImmutableBiMap;
 
 import org.chalup.thneed.ModelGraph;
-import org.chalup.thneed.ModelVisitor;
 import org.chalup.thneed.models.DatabaseModel;
 import org.chalup.thneed.models.PojoModel;
 
 public final class ClassToTable<TModel extends DatabaseModel & PojoModel> {
-
-  private static <TModel extends DatabaseModel & PojoModel> ImmutableBiMap<Class<?>, String> buildClassToTableMap(ModelGraph<TModel> modelGraph) {
-    final ImmutableBiMap.Builder<Class<?>, String> classToTableMappingBuilder = ImmutableBiMap.builder();
-    modelGraph.accept(new ModelVisitor<TModel>() {
-      @Override
-      public void visit(TModel model) {
-        classToTableMappingBuilder.put(model.getModelClass(), model.getTableName());
-      }
-    });
-    return classToTableMappingBuilder.build();
-  }
-
   private final ImmutableBiMap<Class<?>, String> mClassToTableBiMap;
 
   public ClassToTable(ModelGraph<TModel> modelModelGraph) {
-    mClassToTableBiMap = buildClassToTableMap(modelModelGraph);
+    mClassToTableBiMap = Utils.buildClassToTableMap(modelModelGraph);
   }
 
   public String getTableForClass(Class<?> klass) {
