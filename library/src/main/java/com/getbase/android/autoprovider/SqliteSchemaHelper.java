5bd5a913329f6bcecd48fca948c151b9e5a1b1d4
Maciej Witowski <witowski.maciej@gmail.com>
2020-12-16 17:03:08 +0100

    Make classes depend on AutoProviderDatabase interface rather than SQLiteOpenHelper directly
    This allows a different implementation in the client app (eg. managing concurrency of DB operating using ReadWriteLock)

--- library/src/main/java/com/getbase/android/autoprovider/SqliteSchemaHelper.java
+++ library/src/main/java/com/getbase/android/autoprovider/SqliteSchemaHelper.java
@@ -1,22 +1,26 @@
 package com.getbase.android.autoprovider;
 
+import android.database.sqlite.SQLiteDatabase;
+
 import com.getbase.android.sqlitemaster.SQLiteMaster;
 import com.getbase.android.sqlitemaster.SQLiteSchemaPart;
 import com.getbase.android.sqlitemaster.SQLiteSchemaPartType;
+import com.google.common.base.Function;
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
-import android.database.sqlite.SQLiteOpenHelper;
-
+import java.util.List;
 import java.util.Locale;
+
+import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 
 public class SqliteSchemaHelper {
 
-  private final SQLiteOpenHelper mDatabase;
+  private final AutoProviderDatabase mDatabase;
 
-  public SqliteSchemaHelper(SQLiteOpenHelper database) {
+  public SqliteSchemaHelper(AutoProviderDatabase database) {
     mDatabase = database;
   }
 
@@ -24,7 +28,7 @@ public class SqliteSchemaHelper {
     @Override
     public ImmutableSet<String> get() {
       ImmutableSet.Builder<String> builder = ImmutableSet.builder();
-      for (SQLiteSchemaPart part : SQLiteMaster.getSQLiteSchemaParts(mDatabase.getReadableDatabase(), SQLiteSchemaPartType.TABLE)) {
+      for (SQLiteSchemaPart part : getSQLiteSchemaParts(SQLiteSchemaPartType.TABLE)) {
         builder.add(part.name.toLowerCase(Locale.US));
       }
       return builder.build();
@@ -35,12 +39,22 @@ public class SqliteSchemaHelper {
     @Override
     public ImmutableMap<String, String> get() {
       ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
-      for (SQLiteSchemaPart part : SQLiteMaster.getSQLiteSchemaParts(mDatabase.getReadableDatabase(), SQLiteSchemaPartType.VIEW)) {
+      for (SQLiteSchemaPart part : getSQLiteSchemaParts(SQLiteSchemaPartType.VIEW)) {
         builder.put(part.name.toLowerCase(Locale.US), part.sql.toLowerCase(Locale.US));
       }
       return builder.build();
     }
   });
+
+  private List<SQLiteSchemaPart> getSQLiteSchemaParts(final SQLiteSchemaPartType partType) {
+    return mDatabase.execute(new Function<SQLiteDatabase, List<SQLiteSchemaPart>>() {
+      @NullableDecl
+      @Override
+      public List<SQLiteSchemaPart> apply(@NullableDecl SQLiteDatabase input) {
+        return SQLiteMaster.getSQLiteSchemaParts(input, partType);
+      }
+    });
+  }
 
   public ImmutableSet<String> getTables() {
     return mTables.get();
